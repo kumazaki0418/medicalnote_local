1445f94f664e538390f8a5152adec1229ee743ba
熊崎慎之介 <skmzk418.scammyman@gmail.com>
2018-11-28 20:25:59 +0900

    インクリメンタルサーチがsubmitされないように修正
    インクリメント部分を囲っていた<form>を<div class="serch_section">に変更し、submitされないよう変更

--- js/layout.js
+++ js/layout.js
@@ -71,32 +71,6 @@ $(function() {
                   beforeScroll();
                 }
               }
-              if (document.getElementById('index')) {
-                $("section").each(function() {
-                  var sectionimgPos = $(this).offset().top;
-                  var sectionscroll = $(window).scrollTop();
-                  var windowHeight = $(window).height();
-                  if (sectionscroll + 90 > sectionimgPos - windowHeight + windowHeight) {
-                    var setClass = $(this).attr('id');
-                    $('body').removeClass('main topConcept product01 product02 product03 product04 product05 product06 topGift topCF topJournal instagram');
-                    $('body').addClass(setClass);
-                  };
-                });
-
-                $(".effect").each(function() {
-                  var imgPos = $(this).offset().top;
-                  var scroll = $(window).scrollTop();
-                  var windowHeight = $(window).height();
-                  if (scroll > imgPos - windowHeight + windowHeight / 5) {
-                    var animateType = $(this).attr('animate');
-                    $(this).css({
-                      'opacity': '1'
-                    });
-                    $(this).addClass(animateType);
-                  };
-                });
-
-              }
             }
           });
         };
